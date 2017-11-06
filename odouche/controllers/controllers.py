ee320869da9c86de6701786289c7bc064e5854f3
Fabien Meghazi <agr@amigrave.com>
2017-11-06 14:15:11 +0100

    Add scaffold module

---
+++ odouche/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+class Odouche(http.Controller):
+    @http.route('/odouche/odouche/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+
+    @http.route('/odouche/odouche/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('odouche.listing', {
+            'root': '/odouche/odouche',
+            'objects': http.request.env['odouche.odouche'].search([]),
+        })
+
+    @http.route('/odouche/odouche/objects/<model("odouche.odouche"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('odouche.object', {
+            'object': obj
+        })
